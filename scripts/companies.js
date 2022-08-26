397e058f5c271d980a91998a5d0dd995e3687301
MattiaBebe <mattia.bertinelli@cypag-group.com>
2022-08-26 09:58:08 +0200

    add keap connection

--- scripts/companies.js
+++ scripts/companies.js
@@ -1,11 +1,25 @@
 const utils = require('../utils');
+const axios = require('axios');
 
 module.exports = async () => {
     const companies = await utils.readCsvFile('db_migration/aziende.csv');
+    
+    // const config = {
+        // method: 'get',
+        url = `https://api.infusionsoft.com/crm/rest/v1/account/profile?access_token=QAT2kIAGbapjIzUnfakAHBxzDnYV`;
+        headers = { 
+            'Cookie': '__cf_bm=zsTKMaN_.RXBUyunsVknDrj1y2ZtvQE74VJG6Nnzea8-1661498500-0-ASt8sv/NlrORHdRnKLLafnTclhfh6oxv+Dah40Z3oPYrwzNzlzkpPeN2DPRYVF+i7QnFDPdwhYAFIJpyiWciqSM=; GCLB=CLmigoTe44DJIg; JSESSIONID=DC7D4C0E8DC908581452AB636FD97393'
+        }
+        console.log(url);
+    // }
+    try{
+        const res = await axios.get(url, headers);
+        console.log(res);
+    }
+    catch(err){
+        console.error(err.message)
+    }
 
-    // companies.map(c => {
-    //     console.log(c);
-    // })
 
     console.log(companies.join(','))
     return true;
