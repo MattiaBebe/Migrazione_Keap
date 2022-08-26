7f358c3ece730c59d95c570291a584eecfcd2908
MattiaBebe <mattia.bertinelli@cypag-group.com>
2022-08-26 15:03:24 +0200

    stable code with error contemplation

--- scripts/companies.js
+++ scripts/companies.js
@@ -6,6 +6,7 @@ const VALID_COMPANIES_ROLES = [
 ]
 
 let isoCountries;
+let apiErrors = [];
 
 const buildKeapCompany = (c4cCompany) => {
     const isoCountry = isoCountries.find(i => i['alpha-2'] === c4cCompany.CountryRegion);
@@ -13,8 +14,8 @@ const buildKeapCompany = (c4cCompany) => {
         address: {
             country_code: isoCountry ? isoCountry['alpha-3'] : null,
             line1: c4cCompany.Street + `${c4cCompany.House_Number ? ', ' + c4cCompany.House_Number : ''}`,
-            line2: c4cCompany.City,
-            locality: c4cCompany.CountryRegion_Text,
+            // line2: c4cCompany.City,
+            locality: c4cCompany.City,
             zip_code: c4cCompany.Postal_Code && c4cCompany.Postal_Code.length === 5 ? c4cCompany.Postal_Code : null,
             zip_four: c4cCompany.Postal_Code && c4cCompany.Postal_Code.length === 4 ? c4cCompany.Postal_Code : null,
         },
@@ -60,35 +61,78 @@ module.exports = async () => {
         }
     }
     catch(err){
-        console.error(err.message)
+        errore = {
+            ...err, 
+            type: 'get companies error'
+        };
+        apiErrors.push(errore);
     }
 
-    const validC4cCompanies = c4cCompanies.filter(c => VALID_COMPANIES_ROLES.includes(c.Role));
-    const migrationCompanies = validC4cCompanies.map(c => buildKeapCompany(c));
-
-    const companiesToInsert = migrationCompanies.filter(c => !keapCompanies.map(k => k.company_name.toUpperCase()).includes(c.company_name.toUpperCase())).slice(0,1);
-    const companiesToUpdate = migrationCompanies.filter(c => keapCompanies.map(k => k.company_name.toUpperCase()).includes(c.company_name.toUpperCase())).slice(0,1);
-
-    utils.saveJson(keapCompanies, `keapCompanies_${(new Date()).valueOf()}`, 'results');
-
-    for(c of companiesToInsert) {
-        try {
-            const data = JSON.stringify(c);
-            const config = {
-                method: 'post',
-                url: `${process.env.KEAP_API_URL}/companies?access_token=${process.env.KEAP_ACCESS_TOKEN}`,
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                data : data
+    if(apiErrors.length === 0){
+        const validC4cCompanies = c4cCompanies.filter(c => VALID_COMPANIES_ROLES.includes(c.Role));
+        const migrationCompanies = validC4cCompanies.map(c => buildKeapCompany(c));
+    
+        const companiesToInsert = migrationCompanies.filter(c => !keapCompanies.map(k => k.company_name.toUpperCase()).includes(c.company_name.toUpperCase())).slice(0,1);
+        const companiesToUpdate = migrationCompanies.filter(c => keapCompanies.map(k => k.company_name.toUpperCase()).includes(c.company_name.toUpperCase())).slice(0,1);
+        
+        // dev only
+        // utils.saveJson(keapCompanies, `keapCompanies_${(new Date()).valueOf()}`, 'results');
+    
+        for(c of companiesToInsert) {
+            try {
+                const data = JSON.stringify(c);
+                const config = {
+                    method: 'post',
+                    url: `${process.env.KEAP_API_URL}/companies?access_token=${process.env.KEAP_ACCESS_TOKEN}`,
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    data : data
+                }
+                const res = await axios(config);
+                console.log(res);
             }
-            const res = await axios(config);
-            console.log(res);
+            catch (err) {
+                errore = {
+                    ...err,
+                    type: 'insert company error',
+                    data: c
+                };
+                apiErrors.push(errore);
+            }
         }
-        catch (err) {
-            console.error(err)
-        }
+    
+        for(c of companiesToUpdate){
+            const companyId = keapCompanies.find(k => k.company_name === c.company_name).id;
+            try{
+                const data = JSON.stringify(c);
+                const config = {
+                    method: 'patch',
+                    url: `${process.env.KEAP_API_URL}/companies/${companyId}?access_token=${process.env.KEAP_ACCESS_TOKEN}`,
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    data : data
+                }
+                const res = await axios(config);
+                console.log(res);
+            }
+            catch(err){
+                errore = {
+                    ...err,
+                    type: 'update company error',
+                    data: c
+                };
+                apiErrors.push(errore);
+            }
+        } 
+    }
+    
+    const status = apiErrors.length === 0;
+
+    if(!status){
+        utils.saveJson(apiErrors, `companiesScriptErrors_${(new Date()).valueOf()}`, 'results');
     }
 
-    return true;
+    return status;
 }
